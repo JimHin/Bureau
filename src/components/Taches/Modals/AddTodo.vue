f255c3a80cbc15f6e7b8fbf8b888c98a977cb56c
JimHin <krowstudio@icloud.com>
2021-02-27 12:49:11 +0100

    Ajout des commentaires

--- src/components/Taches/Modals/AddTodo.vue
+++ src/components/Taches/Modals/AddTodo.vue
@@ -1,95 +1,109 @@
+<!-- Composant AddTodo (<add-todo/> dans le composant parent). Il est chargé d'afficher le formulaire permettant d'ajouter une tâche à la liste -->
 <template>
     <q-card>
-    <q-card-section class="row">
-        <div class="text-h6">Ajouter une Tâche</div>
-        <q-space/>
-        <q-btn
-        v-close-popup
-        flat
-        round
-        dense
-        icon="close"
-        >
-
-        </q-btn>
-    </q-card-section>
-    <form 
-    @submit.prevent="soumettreFormulaire"
-    >
-        <q-card-section class="q-pt-none">
-            <div class="row q-mb-sm">
-                <q-input 
-                outlined 
-                v-model="tacheASauvegarder.nom"
-                :rules="[val => !!val || 'Le nom est requis']"
-                ref="nom" 
-                label="Nom de la tâche" 
-                />
-            </div>
-            
-
-        
-            <div class="row q-mb-sm">
-                <q-input
-                outlined 
-                v-model="tacheASauvegarder.dateEcheance"
-                ref="date"
-                label="date d'échéance" 
-                >
-                    <template v-slot:append>
-                        <q-icon name="event" class="cursor-pointer">
-                        <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
-                            <q-date v-model="tacheASauvegarder.dateEcheance">
-                            <div class="row items-center justify-end">
-                                <q-btn v-close-popup label="Close" color="primary" flat />
-                            </div>
-                            </q-date>
-                        </q-popup-proxy>
-                        </q-icon>
-                    </template>
-                </q-input>
-            </div>
-            <div class="row q-mb-sm">
-                <q-input 
-                outlined 
-                v-model="tacheASauvegarder.heureEcheance"
-                ref="time"
-                label="heure d'échéance" 
-                >
-                    <template v-slot:append>
-                    <q-icon name="access_time" class="cursor-pointer">
-                        <q-popup-proxy transition-show="scale" transition-hide="scale">
-                        <q-time v-model="tacheASauvegarder.heureEcheance">
-                            <div class="row items-center justify-end">
-                            <q-btn v-close-popup label="Close" color="primary" flat />
-                            </div>
-                        </q-time>
-                        </q-popup-proxy>
-                    </q-icon>
-                    </template>
-                </q-input> 
-            </div>
-
-
+        <q-card-section class="row">
+            <!-- Titre du Modal -->
+            <div class="text-h6">Ajouter une Tâche</div>
+            <!-- Le composant q-space permet de séparer le titre et le bouton de fermeture du modal -->
+            <q-space/>
+            <!-- Bouton de fermeture du modal avec une icone en forme de croix (close) -->
+            <q-btn
+            v-close-popup
+            flat
+            round
+            dense
+            icon="close"
+            >
+            </q-btn>
 
         </q-card-section>
+        <!-- Formulaire d'ajout d'une Tâche. On ajoute prevent derrière l'appel à submit pour que la page ne se recharge pas après la soumission du formulaire -->
+        <form 
+        @submit.prevent="soumettreFormulaire"
+        >
+            <q-card-section class="q-pt-none">
+                <!-- Entrée permettant la saisie du nom de la Tâche -->
+                <div class="row q-mb-sm">
+                    <q-input 
+                    outlined 
+                    v-model="tacheASauvegarder.nom"
+                    :rules="[val => !!val || 'Le nom est requis']"
+                    ref="nom" 
+                    label="Nom de la tâche" 
+                    />
+                </div>
+                
 
-        <q-card-actions align="right">
-            <q-btn 
-            label="Sauvegarder" 
-            color="primary" 
-            v-close-popup 
-            type="submit"
-            />
-        </q-card-actions> 
-    </form>
+                <!-- Entrée permettant la saisie de la date d'échéance prévue de la Tâche -->
+                <div class="row q-mb-sm">
+                    <q-input
+                    outlined 
+                    v-model="tacheASauvegarder.dateEcheance"
+                    ref="date"
+                    label="date d'échéance" 
+                    >
+                        <template v-slot:append>
+                            <q-icon name="event" class="cursor-pointer">
+                            <!-- Popup permettant d'afficher un Date Picker -->
+                            <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
+                                <q-date v-model="tacheASauvegarder.dateEcheance">
+                                <div class="row items-center justify-end">
+                                    <q-btn v-close-popup label="Close" color="primary" flat />
+                                </div>
+                                </q-date>
+                            </q-popup-proxy>
+                            </q-icon>
+                        </template>
+                    </q-input>
+                </div>
+                <!-- Entrée permettant la saisie de l'heure d'échéance prévue de la Tâche -->
+                <div class="row q-mb-sm">
+                    <q-input 
+                    outlined 
+                    v-model="tacheASauvegarder.heureEcheance"
+                    ref="time"
+                    label="heure d'échéance" 
+                    >
+                        <template v-slot:append>
+                        <q-icon name="access_time" class="cursor-pointer">
+                            <!-- Popup permettant d'afficher un Time Picker -->
+                            <q-popup-proxy transition-show="scale" transition-hide="scale">
+                            <q-time v-model="tacheASauvegarder.heureEcheance">
+                                <div class="row items-center justify-end">
+                                <q-btn v-close-popup label="Close" color="primary" flat />
+                                </div>
+                            </q-time>
+                            </q-popup-proxy>
+                        </q-icon>
+                        </template>
+                    </q-input> 
+                </div>
+
+
+
+            </q-card-section>
+            <!-- La card action et son bouton permettant l'envoi du formulaire -->
+            <q-card-actions align="right">
+                <q-btn 
+                label="Sauvegarder" 
+                color="primary" 
+                v-close-popup 
+                type="submit"
+                />
+            </q-card-actions> 
+
+        </form>
+
     </q-card>
 </template>
 <script>
+// Import de mapActions permettant d'accéder aux actions contenu dans le store ( Ici on veut avoir accés à ajouterTache() )
 import { mapActions } from "vuex";
+// Export du composant. Ici je ne lui ai pas donné de nom
 export default {
     data (){
         return{
+            // Objet représentant le modèle de données d'une tâche
             tacheASauvegarder:{
                 nom: '',
                 accomplie: false,
@@ -99,15 +113,21 @@ export default {
         }
     },
     methods:{
+        // On map les actions provenant du store. Ici on a besoin de l'action ajouterTache()
         ...mapActions('taches', ['ajouterTache']),
+        // Méthode permettant de valider les données du formulaire et d'appeler soumettreTache s'ils ne renvoient pas d'erreur
         soumettreFormulaire(){
             this.$refs.nom.validate()
             if (!this.$refs.nom.hasError) {
                 this.soumettreTache()
             }
         },
+        // Méthode qui fait appel à l'action ajouterTache
         soumettreTache(){
+            // Appel à ajouterTache en lui passant la tache à sauvegarder en paramètre
             this.ajouterTache(this.tacheASauvegarder)
+            // On emet un évènement que je nomme ici "fermer" permettant de signifier la fermeture du modal
+            this.$emit('fermer')
         }
     }
 }
--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -2,10 +2,13 @@
   <q-page
   class="q-pa-md bg-dark"
   >
+    <!-- composant list permettant d'afficher la liste des tâches -->
     <q-list
     separator 
     bordered>
+      <!-- Titre de la liste  -->
       <q-item-label header>Liste des Tâches</q-item-label>
+      <!-- Parcours de l'objet taches pour itérer les tâches -->
       <tache
         v-for="(tache, key ) in taches"
         :key='key'
@@ -13,7 +16,9 @@
         :id="key"
       ></tache>
     </q-list>
+    <!-- Conteneur de placement du bouton ajouter -->
     <div class="absolute-bottom text-center q-mb-lg">
+      <!-- Bouton avec une icône + permettant d'afficher le modal contenant le formulaire d'ajout exprimé ci-dessous -->
       <q-btn
       @click="showAjouterTache = !showAjouterTache"
       round
@@ -23,7 +28,7 @@
       >
       </q-btn>
     </div>
-
+    <!--Appel au composant AddTodo.vue dans une balise q-dialog qui s'appuie sur le booléen showAjouterTache pour s'afficher ou non -->
     <q-dialog v-model="showAjouterTache">
       <add-todo/>
     </q-dialog>
@@ -32,8 +37,10 @@
 </template>
 
 <script>
+// import des deux composants nécessaires pour accéder aux tâches et au formulaire d'ajout
 import Tache from 'src/components/Taches/Tache.vue'
 import AddTodo from 'src/components/Taches/Modals/AddTodo'
+// import des Getters contenu dans le store
 import { mapGetters } from 'vuex'
 export default {
   name: 'PageTodo',
@@ -43,9 +50,11 @@ export default {
     }
   },
   computed: {
+    // On permet ici à PageTodo.vue d'accéder aux getters du store
     ...mapGetters('taches', ['taches'])
   },
   components: {
+    // Après leurs import on déclare nos deux composants comme étant embarqués dans celui-ci
     Tache,
     AddTodo
   }
