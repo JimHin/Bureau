452afdc5328cef830c69323c7b70dbbba7844e0c
JimHin <krowstudio@icloud.com>
2021-03-05 14:36:30 +0100

    Séparation de la liste des tâches en deux listes distinctes "A faire" - "Fini". Affichage d'une bannière "Aucune tâche à accomplir aujourd'hui" avec utilisation du bus global pour afficher le formulaire de saisie d'une nouvelle tâche à partir du nouveau bouton de cette bannière.

---
+++ src/components/Taches/AucuneTache.vue
@@ -0,0 +1,26 @@
+<template>
+    <q-banner class="bg-grey-3">
+      <template v-slot:avatar>
+        <q-icon name="check" color="primary" />
+      </template>
+      Aucune Tâche à accomplir aujourd'hui
+      <template v-slot:action>
+        <q-btn 
+        flat 
+        color="primary" 
+        label="Ajouter une tâche"
+        @click="$root.$emit('showAjouterTache')" 
+        />
+      </template>
+    </q-banner>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Taches/TacheARealiser.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+    <entete-liste
+    bgColor="bg-orange-4"
+    >
+    A Faire
+    </entete-liste>
+    <!-- composant list permettant d'afficher la liste des tâches non accomplies -->
+    <q-list
+    separator 
+    bordered
+    >
+      <!-- Parcours de l'objet tachesARealiser pour itérer les tâches -->
+      <tache
+        v-for="(tache, key ) in tachesARealiser"
+        :key='key'
+        :tache="tache"
+        :id="key"
+      ></tache>
+    </q-list>
+</div>
+   
+</template>
+
+<script>
+import Tache from 'src/components/Taches/Tache.vue'
+import EnteteListe from '../Shared/EnteteListe.vue'
+export default {
+    name: "TacheARealiser",
+    props: ['tachesARealiser'],
+    components: {
+        Tache,
+        EnteteListe
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
